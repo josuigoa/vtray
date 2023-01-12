2c0e12e5b6571ddd84ad0a541c8cb718a4986a43
Josu Igoa <josu.igoa@indaba.lks.es>
2023-01-12 08:09:26 +0100

    simplify example

--- main.v
+++ main.v
@@ -1,77 +1,43 @@
 $if linux {
-	// #define TRAY_APPINDICATOR 1
 	#flag -DTRAY_APPINDICATOR=1 $(shell pkg-config --cflags appindicator3-0.1) $(shell pkg-config --libs appindicator3-0.1)
 }
 $if windows {
-	// #define TRAY_WINAPI 1
 	#flag -DTRAY_WINAPI=1
 }
 $if macos {
-	// #define TRAY_APPKIT 1
 	#flag -DTRAY_APPKIT=1 -framework Cocoa
 }
 
 #flag -I. -Wall -Wextra -std=c99
 #include "tray.h"
-#include "@VMODROOT/tray.c"
 
-fn C.tray_init(TrayParams) &TrayInfo
-fn C.tray_loop(&TrayInfo)
-fn C.tray_run(&TrayInfo)
-fn C.tray_exit(&TrayInfo)
-
-struct TrayMenuItem {
-	id   string [required] // Unique ID.
-	text string [required] // Text to display.
+struct C.tray {
+	icon &u8
+	menu &C.tray_menu
 }
 
-// Parameters to configure the tray button.
-struct TrayParams {
-	items    []TrayMenuItem         [required]
-	on_click fn (item TrayMenuItem)
+struct C.tray_menu {
+pub:
+	text &u8
 }
 
-// Internal Cocoa application state.
-struct TrayInfo {
-	app          voidptr // pointer to NSApplication
-	app_delegate voidptr // pointer to AppDelegate
-}
-
-[heap]
-struct MyApp {
-mut:
-	tray_info &TrayInfo = unsafe { nil }
-}
-
-fn (app &MyApp) on_menu_item_click(item TrayMenuItem) {
-	println('click ${item.id}')
-	if item.id == 'quit' {
-		C.tray_exit(app.tray_info)
-	}
-}
+fn C.tray_init(&C.tray) int
+fn C.tray_loop(int) int
 
 fn main() {
-	mut my_app := &MyApp{
-		tray_info: 0
+	tray := &C.tray{
+		icon: 'icon.ico'.str
+		menu: &C.tray_menu{
+			text: 'text'.str
+		}
 	}
-
-	my_app.tray_info = C.tray_init(TrayParams{
-		items: [TrayMenuItem{
-			id: 'hello'
-			text: 'Hello'
-		}, TrayMenuItem{
-			id: 'quit'
-			text: 'Quit!'
-		}]
-		on_click: my_app.on_menu_item_click
-	})
-
-	//// Use this:
-	// for {
-	// 	C.tray_loop(my_app.tray_info)
-	//  	// println("loop")
-	// }
-
-	//// Or this:
-	C.tray_run(my_app.tray_info)
+	
+	C.tray_init(tray)
+	for {
+		print(C.tray_loop(1))
+		if C.tray_loop(1) != 0 {
+			break
+			// printf("iteration\n");
+		}
+	}
 }
